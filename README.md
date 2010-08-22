50fd17ff7326ee6d5f689a54c74a3559858bb917
Lee Boynton <lee@boynton.com>
2010-08-21 20:08:19 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Template
+
+Herein lies a really simple template engine.
+
+> WORK IN PROGRESS.
+
+## Related things
+
+* http://github.com/creationix/microtemplates - My motivation.
+* http://github.com/ry/node - My context.
+* http://github.com/defunkt/mustache - Cool, but not my style.
+
+## Syntax
+
+## Usage
+
+
---
+++ examples/ex1.js
@@ -0,0 +1,21 @@
+var template = require("template");
+
+var dat = {
+    title: "The Title",
+    foo: {
+        author: "Lee"
+    },
+    error: "Cannot solve world hunger.",
+    list: [
+        {href: "/href/1", text: "One"},
+        {href: "/href/2", text: "Two"},
+        {href: "/href/3", text: "Three"},
+    ]
+}
+
+function cleanString(s) {
+    return s.replace(/\n/g, '\\n').replace(/'/g, "\\'");
+}
+
+var tmp = template.fromFile('ex1.tmpl');
+console.log(tmp(dat));
